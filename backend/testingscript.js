9defa51263fbc51aa47aaf5d667a35fc80c900cb
Abhishek Kumar Gupta <abhishekprince9109@gmail.com>
2026-02-06 21:41:53 +0530

    video call working

--- backend/testingscript.js
+++
@@ -1,8 +0,0 @@
-
-const listModels = async () => {
-  const { GoogleGenerativeAI } = require("@google/generative-ai");
-  const genAI = new GoogleGenerativeAI("AIzaSyAjPt-JGgPwEu06rUU9jGtTOQI_gPST6pw");
-  const result = await genAI.listModels();
-  console.log(result);
-};
-listModels();
